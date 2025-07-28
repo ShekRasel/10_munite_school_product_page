466c30af1d716ca84099675f837ac8891769e813
Shek Rasel <swe.rasel@gmail.com>
2025-07-28 18:01:52 +0600

    config file change

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "cdn.10minuteschool.com",
-      "cdn.10minuteschool.com",
-      "s3.ap-southeast-1.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.10minuteschool.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.ap-southeast-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
